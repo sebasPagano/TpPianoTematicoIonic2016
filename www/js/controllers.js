542cdf75ee833b0e8c28cd1363af255d0f790fb4
sebasPagano <sebas_el14@live.com.ar>
2016-09-16 22:15:49 -0300

    Comenzando piano

---
+++ www/js/controllers.js
@@ -0,0 +1,52 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats,$timeout) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+$scope.MisMensajes=[];
+
+ var messagesRef = new Firebase('https://tppiano-c5592.firebaseio.com/usuarios/');
+
+
+
+  var name = $('#nameInput').val();
+  messagesRef.push({usuario:name});
+    
+  
+
+ messagesRef.on('child_added', function (snapshot) {
+  $timeout(function(){
+
+    var message = snapshot.val();
+    $scope.MisMensajes.push(message);
+    console.log($scope.MisMensajes);
+    });
+
+  });
+
+
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
